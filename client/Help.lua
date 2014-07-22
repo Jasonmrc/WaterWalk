83f13ba8c6a54d5ed2d0599ee302eb3d059b6cf7
Jasonmrc <jasonmrc@gmail.com>
2014-07-21 23:46:02 -0400

    Edited Help

--- client/Help.lua
+++ client/Help.lua
@@ -3,10 +3,10 @@ function ModulesLoad()
         {
             name = "WaterWalk",
             text = 
-                "WaterWalk allows you to walk, drive, or land a plane or helicopter on water.\n"..
-                "Boats are not affected by this and you can easily enter a boat and it will operate like normal-\n"..
-                "Or exit a boat and stand on the water.\n"..
-                "Use '/waterwalk' to enable and disable it.\n"..
+				"WaterWalk allows you to walk, drive, or land a plane or helicopter on water.\n"..
+				"Boats are not affected by this and you can easily enter a boat and it will operate like normal-\n"..
+				"Or exit a boat and stand on the water.\n"..
+				"Use '/waterwalk' to enable and disable it.\n"..
 				"\n:: WaterWalk was developed by JasonMRC of Problem Solvers.\n"
         } )
 end
